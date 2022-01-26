99439ca14b939baa857a42279e06eafb4eb7974d
Valentin Roche <54205303+titithen00b@users.noreply.github.com>
2022-01-26 12:24:00 +0100

    Update sqlquerykiller.sh

--- sqlquerykiller.sh
+++ sqlquerykiller.sh
@@ -6,4 +6,22 @@ DBPASSWD=
 DBPORT=
 
 
-mysql $DBUSER $DBPASSWD $DBHOST $DBPORT -e "select concat('KILL ',id,';')  from information_schema.processlist where  INFO like 'SELECT%' and time >= 150;"
+#init boucle while
+/bin/systemctl -q is-active "requestkillerSQL.service"
+status=$?
+
+#start boucle while
+
+
+
+while [[ $status == 0 ]]
+	do
+		mysql $DBUSER $DBPASSWD $DBHOST $DBPORT -e "select concat('KILL ',id,';')  from information_schema.processlist where  INFO like 'SELECT%' and time >= 150;"
+		
+    #test service active
+    /bin/systemctl -q is-active "requestkillerSQL.service"
+		status=$?
+    
+    #exec all seconds
+		sleep 1
+	done
