d7fd6058d9cbd8a38ddb0b2ab18b3930fdf3ba70
Valentin Roche <54205303+titithen00b@users.noreply.github.com>
2025-02-07 15:21:08 +0100

    Update sqlquerykiller.sh

--- sqlquerykiller.sh
+++ sqlquerykiller.sh
@@ -1,27 +1,63 @@
 #!/bin/bash
 
-DBHOST=
-DBUSER=
-DBPASSWD=
-DBPORT=
+host=""
+password=""
+user=""
+port=""
 
+wanttime=180
 
 #init boucle while
 /bin/systemctl -q is-active "requestkillerSQL.service"
 status=$?
 
+timestamp=$(date +"%d-%m-%y_%H:%M")
+
+listsqlrequest(){
+    resultat=`mysql -u "$user" -p"$password" -h "$host" -P "$port" -e "select concat('KILL ',id,';') from information_schema.processlist where INFO like 'SELECT%' AND TIME >='$wanttime';"`
+
+    return 0
+}
+
+
+killrequest(){
+    kill=$(echo $resultat | cut -c 24-)
+    echo $kill 
+    return 0
+}
+
+
+killcom(){
+    if [[ "$kill" == *KILL* ]]
+    then mysql -u "$user" -p"$password" -h "$host" -P "$port" -e "$kill"
+    else echo "[$timestamp] Aucune requête trouvé avec le temps maximal indiqué : $wanttime seconde(s)" >> /var/log/killerrequests
+    fi
+
+}
+
+stopservice(){
+    systemctl stop "requestkillerSQL.service"
+    echo "[$timestamp] Le service a été arreté."
+}
+
+
 #start boucle while
-
-
-
 while [[ $status == 0 ]]
-	do
-		mysql $DBUSER $DBPASSWD $DBHOST $DBPORT -e "select concat('KILL ',id,';')  from information_schema.processlist where  INFO like 'SELECT%' and time >= 150;"
-		
-    #test service active
-    /bin/systemctl -q is-active "requestkillerSQL.service"
-		status=$?
-    
-    #exec all seconds
-		sleep 1
-	done
+do
+
+    if [[ $wanttime -ge 1 ]]; then 
+        listsqlrequest
+        killrequest
+        killcom
+    else 
+        stopservice
+        echo "[$timestamp] Merci de rentrer un temps d'execution (en seconde) valide" >> /var/log/killerrequests
+        exit
+    fi
+#test service active    
+/bin/systemctl -q is-active "requestkillerSQL.service"
+    status=$?
+
+#exec all 10 seconds
+    sleep 10
+done
